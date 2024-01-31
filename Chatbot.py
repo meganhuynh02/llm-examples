accdb3faf30df2c173a7ec37bdbaaf7019386a60
Megan Huynh <62710899+meganhuynh02@users.noreply.github.com>
2024-01-31 11:40:47 -0800

    adding file input

--- Chatbot.py
+++ Chatbot.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
 import streamlit as st
+from io import StringIO
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
@@ -24,7 +25,23 @@ if prompt := st.chat_input():
     #if picture:
         #st.image(picture)
     
-    uploaded_file = st.file_uploader("Or use an existing image")
+    uploaded_file = st.file_uploader("Choose a file")
+    if uploaded_file is not None:
+        # To read file as bytes:
+        bytes_data = uploaded_file.getvalue()
+        st.write(bytes_data)
+
+        # To convert to a string based IO:
+        stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
+        st.write(stringio)
+
+        # To read file as string:
+        string_data = stringio.read()
+        st.write(string_data)
+
+        # Can be used wherever a "file-like" object is accepted:
+        dataframe = pd.read_csv(uploaded_file)
+        st.write(dataframe)
 
     client = OpenAI(api_key=openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
