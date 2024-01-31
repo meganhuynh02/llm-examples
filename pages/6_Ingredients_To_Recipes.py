29f8bb369da559d8ceb67a4b2c2ed3edde3d0c78
Megan Huynh <62710899+meganhuynh02@users.noreply.github.com>
2024-01-31 11:57:39 -0800

    adding user text input for the chatbox

--- pages/6_Ingredients_To_Recipes.py
+++ pages/6_Ingredients_To_Recipes.py
@@ -25,6 +25,25 @@ uploaded_file = st.file_uploader("Or upload an existing picture")
 if uploaded_file and not openai_api_key:
     st.info("Please add your OpenAI API key to continue.")
 
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    picture = st.camera_input("Take a picture of the ingredients you have")
+    if picture:
+        st.image(picture)
+
+    client = OpenAI(api_key=openai_api_key)
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    response = client.chat.completions.create(model="gpt-4-vision-preview", messages=st.session_state.messages)
+    msg = response.choices[0].message.content
+    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.chat_message("assistant").write(msg)
 # if uploaded_file and openai_api_key:
 #     article = uploaded_file.read().decode()
 #     prompt = f"""{anthropic.HUMAN_PROMPT} Here's an article:\n\n<article>
