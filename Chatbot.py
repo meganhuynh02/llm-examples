55485cd2a99efb1e7f5304701ef9ec68032eee4a
Megan Huynh <62710899+meganhuynh02@users.noreply.github.com>
2024-01-31 11:26:38 -0800

    changed message, testing emoji

--- Chatbot.py
+++ Chatbot.py
@@ -10,7 +10,7 @@ with st.sidebar:
 st.title("💬 Ingredients to Recipes")
 st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "What should we cook today :yum:"}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
